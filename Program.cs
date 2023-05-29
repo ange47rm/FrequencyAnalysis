854ea76297c8ae8aa23e825ba4c1e4d34ac0557e
Angelo Di Massimo <Ange47RM@hotmail.com>
2023-05-29 19:50:04 +0100

    Output shows letters and count, but logic needs to be amended

--- Program.cs
+++ Program.cs
@@ -17,18 +17,35 @@
 
                         string noWhiteSpaceText = RemoveWhiteSpace(fileText);
 
-                        Console.WriteLine("Total characters: " + noWhiteSpaceText.Length);
+                        Console.WriteLine("Total characters: " + noWhiteSpaceText);
 
-                        var obj = new LetterCount("a", 13);
+                        // create array with all letters
+                        char[] lettersArray = noWhiteSpaceText.ToCharArray();
 
-                        Console.WriteLine($"{obj.Letter} ({obj.Count})");
+                        // create array of unique letters
+                        char[] uniqueLettersArray = lettersArray.Distinct().ToArray();
+
+                        // create final array of LetterCount objects
+                        List<LetterCount> letterCountList = new List<LetterCount>();
 
                         // for each letter, loop over the array and count how many there are (add case sensitive toggle)
+                        foreach (char letterX in uniqueLettersArray)
+                        {
+                            int count = 0;
+                            foreach (char letterY in lettersArray)
+                            {
+                                if (letterX == letterY) count++;
+                            }
 
-                        // create letter count object and add it to FINAL array (of LetterCount objects)
+                            // create letter count object and add it to FINAL array (of LetterCount objects)
+                            letterCountList.Add(new LetterCount(letterX.ToString(), count));
+                        }
 
                         // then loop over FINAL array each letter count object, and for each one, log letter and count
-                        ;
+                        foreach (LetterCount letterCount in letterCountList)
+                        {
+                            Console.WriteLine($"{letterCount.Letter} ({letterCount.Count})");
+                        }
                     }
                     else
                     {
