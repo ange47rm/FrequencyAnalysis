87589c0ef0fc525c6c620c13ea2c2285031cbd8b
Angelo Di Massimo <Ange47RM@hotmail.com>
2023-05-29 17:54:50 +0100

    Based on the file name provided as an argument, the console outputs text lines in each file correctly

--- Program.cs
+++ Program.cs
@@ -12,6 +12,15 @@
                 if (filePath != null)
                 {
                     Console.WriteLine($"File path: {filePath}");
+
+                    List<string> textLines = new List<string>();
+
+                    textLines = File.ReadAllLines(filePath).ToList();
+
+                    foreach (string line in textLines)
+                    {
+                        Console.WriteLine($"{line}");
+                    }
                 }
                 else
                 {
@@ -28,7 +37,7 @@
             if (args.Length > 0)
             {
                 fileName = args[0];
-                filePath = @$"C:\Code\FrequencyAnalysis\text-files\{fileName}";
+                filePath = @$"C:\Code\FrequencyAnalysis\text-files\{fileName}.txt";
                 return filePath;
             }
             else
