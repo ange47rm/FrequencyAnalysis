c10d1def67f36d0181b56057dfa3aca4225472ec
Angelo <Angelo@SOVEREIGN>
2023-05-29 17:07:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace FrequencyAnalysis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Frequency Analysis application started");
+
+                if (args.Length > 0)
+                {
+                    string arg = args[0];
+                    Console.WriteLine("Args received: " + arg);
+                }
+                else
+                {
+                    Console.WriteLine("No arg received.");
+                }
+            }
+        }
+    }
+}
